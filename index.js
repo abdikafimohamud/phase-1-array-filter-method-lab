5c4db76bf3d7421fb46b77ab96716af78efc9e56
abdikafimohamud <185328738+abdikafimohamud@users.noreply.github.com>
2025-03-13 07:08:53 -0700

    array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+  }
+  
+  function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  const broncosRecord = [
+    { year: "2014", result: "L" },
+    { year: "2015", result: "W" },
+    { year: "2016", result: "L" }
+  ];
+  
+  console.log(superbowlWin(broncosRecord)); 
+  
+  const drivers = ["Sam", "Sarah", "Sally", "Samantha"];
+  console.log(findMatching(drivers, "sAm")); 
+  console.log(fuzzyMatch(drivers, "Sa")); 
+  
+  const driverObjects = [
+    { name: "Bobby", hometown: "New York" },
+    { name: "Sam", hometown: "Nashville" },
+    { name: "Sarah", hometown: "Los Angeles" }
+  ];
+  
+  console.log(matchName(driverObjects, "Sam")); 
